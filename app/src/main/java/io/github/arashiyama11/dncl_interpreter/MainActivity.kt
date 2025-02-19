c1d3c02a90eb792bd1a4309c15da5ddfed9d4fb7
arashiyama11 <arashiyama1120@gmail.com>
2025-02-19 14:37:00 +0900

    Add KoinContext to MainActivity

--- app/src/main/java/io/github/arashiyama11/dncl_interpreter/MainActivity.kt
+++ app/src/main/java/io/github/arashiyama11/dncl_interpreter/MainActivity.kt
@@ -5,13 +5,16 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import io.github.arashiyama11.dncl_interpreter.ui.App
+import org.koin.compose.KoinContext
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            App()
+            KoinContext {
+                App()
+            }
         }
     }
 }
