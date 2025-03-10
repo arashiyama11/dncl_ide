5ef17ffdb525750267fa706a74066a94135c63a8
arashiyama <96863644+arashiyama11@users.noreply.github.com>
2025-03-10 13:50:16 +0900

    improve save timing (#51)

--- app/src/main/java/io/github/arashiyama11/dncl_interpreter/ui/DrawerContent.kt
+++ app/src/main/java/io/github/arashiyama11/dncl_interpreter/ui/DrawerContent.kt
@@ -26,7 +26,6 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.focus.FocusRequester
 import androidx.compose.ui.focus.focusRequester
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import io.github.arashiyama11.dncl_interpreter.adapter.CreatingType
 import io.github.arashiyama11.dncl_interpreter.adapter.DrawerViewModel
